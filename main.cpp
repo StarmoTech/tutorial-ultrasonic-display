b9cddbd25bbde05daf54b331f1639cf10e10f18a
safeboot <mynamesafeboot@gmail.com>
2023-04-09 17:23:16 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,51 @@
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C adresa koju smo skenirali , broj slova, broj redova
+
+int trigPin = 11;  // Trigger
+int echoPin = 12;  // Echo
+long duration, cm, inches;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+  lcd.begin();      // pocetak rada display-a
+  lcd.backlight();  // paljenje pozadinskog svjetla
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  // Konverzija vremena u udaljenost
+
+  // Podijeliti sa 29.1 ili pomnožiti sa 0.0343
+  cm = (duration / 2) * 0.0343;
+
+  //Podijeliti sa 74 ili pomnožiti 0.0135
+  inches = (duration / 2) / 74;  // izbrisite ako ne zelite ince
+
+  if(cm < 100){ // ako je vrijednost manja od 100cm 
+  lcd.setCursor(14, 0); // na mjesto broj 14 (mjesto 15), red 0 (1 red) 
+  lcd.print("  "); // ispiši dva prazna mjesta
+  }
+
+  lcd.setCursor(0, 0);
+  lcd.print("Udaljen. cm ");
+  lcd.print(cm);
+  lcd.setCursor(0, 1);
+  lcd.print("Udaljen. in ");
+  lcd.print(inches);
+  //lcd.clear();
+  delay(1000);
+}
